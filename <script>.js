a8925f5c5b21d32e8bbd7d28a7d4b408c43529cc
Ketevan12 <kirvk209@newschool.edu>
2023-02-10 01:49:49 -0500

    almost done

---
+++ <script>.js
@@ -0,0 +1,46 @@
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Shrikhand&display=swap" rel="stylesheet">
+
+   <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){
+      $("button").click(function(){
+        $("one").toggleClass("main");
+      });
+      $("#two").click(function() {
+    $("#two").toggle();
+});
+    });
+    console.log("I built this website :)");
+    
+    </script>
+    </head>
+    background-image: url(https://www.savethekoala.com/wp-content/uploads/2021/01/1.jpgf);
+.a {
+  box-shadow: inset 0 0 0 0 #54b3d6;
+  color: ;
+  margin: 0 -.25rem;
+  padding: 0 .25rem;
+  transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
+}
+a:hover {
+  box-shadow: inset 100px 0 0 0 #54b3d6;
+  color: white;
+}
+
+</script>
+a {
+    box-shadow: inset 0 0 0 0 #54b3d6;
+    color: pink
+    margin: 0 -.25rem;
+    padding: 0 .25rem;
+    transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
+  }
+a:hover {
+    box-shadow: inset 100px 0 0 0 #54b3d6;
+    color: white;
+  }
+  
